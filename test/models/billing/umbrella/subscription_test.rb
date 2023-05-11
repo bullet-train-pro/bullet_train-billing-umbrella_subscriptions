f674f30c8fb08e9e0a16404c748af716d7bf7c25
Jeremy Green <jeremy@octolabs.com>
2023-05-11 13:41:21 -0500

    write a real(ish) test

--- test/models/billing/umbrella/subscription_test.rb
+++ test/models/billing/umbrella/subscription_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class Billing::Umbrella::SubscriptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Just a starter test to make sure the class is coming up
+  test "you can new up an umbrella subscription" do
+    umbrella_subscription = Billing::Umbrella::Subscription.new
+    assert umbrella_subscription
+  end
+
 end
