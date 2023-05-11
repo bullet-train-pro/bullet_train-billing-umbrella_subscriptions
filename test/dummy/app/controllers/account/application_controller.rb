b3606f309b090d21a6a59e7ed5175213ff62aeb6
Jeremy Green <jeremy@octolabs.com>
2023-05-11 16:16:18 -0500

    trying this

--- test/dummy/app/controllers/account/application_controller.rb
+++ test/dummy/app/controllers/account/application_controller.rb
@@ -1,5 +1,5 @@
 class Account::ApplicationController < ApplicationController
-  def account_load_and_authorize_resource
+  def self.account_load_and_authorize_resource(model, options, old_options = {})
     # an empty method to keep tests happy
   end
 end
