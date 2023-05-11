a5fcd5375faf5f0ddba91f8b0c517a1f9f6f4537
Jeremy Green <jeremy@octolabs.com>
2023-05-11 11:31:53 -0500

    remove a failing test

--- test/bullet_train/billing/test_umbrella_subscriptions.rb
+++ test/bullet_train/billing/test_umbrella_subscriptions.rb
@@ -6,8 +6,4 @@ class BulletTrain::Billing::TestUmbrellaSubscriptions < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::BulletTrain::Billing::UmbrellaSubscriptions::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
