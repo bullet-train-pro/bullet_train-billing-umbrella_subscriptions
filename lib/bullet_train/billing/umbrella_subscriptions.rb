df61c3d62d32a202c9bb7e4f2a9c610cafba0c11
Jeremy Green <jeremy@octolabs.com>
2023-05-15 15:42:25 -0500

    A monkey patch for the product catalog

--- lib/bullet_train/billing/umbrella_subscriptions.rb
+++ lib/bullet_train/billing/umbrella_subscriptions.rb
@@ -1,6 +1,11 @@
 require "bullet_train/billing/umbrella_subscriptions/version"
 require "bullet_train/billing/umbrella_subscriptions/engine"
 
+require "bullet_train/billing"
+require "bullet_train/billing/usage"
+
+require "bullet_train/billing/usage/product_catalog_monkey_patch"
+
 module BulletTrain
   module Billing
     module UmbrellaSubscriptions
