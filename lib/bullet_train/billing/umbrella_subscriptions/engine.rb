c57b762aefa97eba8dde9c01dc9942a455729c88
Jeremy Green <jeremy@octolabs.com>
2023-05-16 09:33:27 -0500

    Wire up the monkeypatch

--- lib/bullet_train/billing/umbrella_subscriptions/engine.rb
+++ lib/bullet_train/billing/umbrella_subscriptions/engine.rb
@@ -2,6 +2,10 @@ module BulletTrain
   module Billing
     module UmbrellaSubscriptions
       class Engine < ::Rails::Engine
+
+        config.to_prepare do
+          ::Billing::Usage::ProductCatalog.prepend(BulletTrain::Billing::Usage::ProductCatalogMonkeyPatch)
+        end
       end
     end
   end
