e748cdc8a631036886dff1c82bd3d6ad4d62c6d1
Jeremy Green <jeremy@octolabs.com>
2023-05-16 14:57:10 -0500

    Gettig started on the UI for pulling an umbrella subscription to a team

---
+++ app/controllers/account/billing/umbrella/subscriptions_controller.rb
@@ -0,0 +1,54 @@
+class Account::Billing::Umbrella::SubscriptionsController < Account::ApplicationController
+  account_load_and_authorize_resource :subscription, through: :team, through_association: :billing_umbrella_subscriptions, member_actions: [:checkout, :refresh, :portal]
+
+  def new
+  end
+
+  # GET/POST /account/billing/stripe/subscriptions/:id/checkout
+  # GET/POST /account/billing/stripe/subscriptions/:id/checkout.json
+  def checkout
+    trial_days = @subscription.generic_subscription.included_prices.map { |ip| ip.price.trial_days }.compact.max
+
+    session_attributes = {
+      payment_method_types: ["card"],
+      subscription_data: {items: @subscription.stripe_items}.merge(trial_days ? {trial_period_days: trial_days} : {}),
+      customer: @team.stripe_customer_id,
+      client_reference_id: @subscription.id,
+      success_url: CGI.unescape(url_for([:refresh, :account, @subscription, session_id: "{CHECKOUT_SESSION_ID}"])),
+      cancel_url: url_for([:account, @subscription.generic_subscription])
+    }
+
+    unless @team.stripe_customer_id
+      session_attributes[:customer_email] = current_membership.email
+    end
+    
+    # Stripe requires that Checkout Sessions having different attributes must
+    # have different idempotency keys, so include the updated_at in the key.
+    idempotency_key = "#{t('application.name')}:subscription:#{@subscription.id}:#{@subscription.updated_at.to_i}"
+
+    session = Stripe::Checkout::Session.create(session_attributes, idempotency_key: idempotency_key)
+
+    redirect_to session.url, allow_other_host: true
+  end
+
+  # POST /account/billing/stripe/subscriptions/:id/portal
+  # POST /account/billing/stripe/subscriptions/:id/portal.json
+  def portal
+    session = Stripe::BillingPortal::Session.create({
+      customer: @team.stripe_customer_id,
+      return_url: url_for([:account, @subscription.generic_subscription])
+    })
+
+    redirect_to session.url, allow_other_host: true
+  end
+
+  # GET /account/billing/stripe/subscriptions/:id/refresh
+  # GET /account/billing/stripe/subscriptions/:id/refresh.json
+  def refresh
+    # If the checkout session is paid already, we want to do a couple things quickly without waiting for a webhook.
+    checkout_session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @subscription.refresh_from_checkout_session(checkout_session)
+
+    redirect_to [:account, @subscription.generic_subscription.team], notice: t("billing/stripe/subscriptions.notifications.refreshed")
+  end
+end
