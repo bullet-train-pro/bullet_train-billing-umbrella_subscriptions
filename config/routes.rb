8510a21f6d974a819d39bbec16be5abf9a8f90f8
Jeremy Green <jeremy@octolabs.com>
2023-05-16 14:56:28 -0500

    import some routes from the Stripe gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,19 @@
 Rails.application.routes.draw do
+
+  namespace :account do
+    shallow do
+      resources :teams, only: [] do
+        namespace :billing do
+          namespace :umbrella do
+            resources :subscriptions
+          end
+          resources :subscriptions, only: [] do
+            namespace :umbrella do
+              resources :subscriptions
+            end
+          end
+        end
+      end
+    end
+  end
 end
