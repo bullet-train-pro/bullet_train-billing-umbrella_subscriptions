03078b584f2ea55c65b67fdee6982e7cfa4422e9
Jeremy Green <jeremy@octolabs.com>
2023-05-18 12:53:20 -0500

    spelling is hard

--- lib/bullet_train/billing/usage/product_catalog_monkey_patch.rb
+++ lib/bullet_train/billing/usage/product_catalog_monkey_patch.rb
@@ -1,11 +1,11 @@
 module BulletTrain::Billing::Usage::ProductCatalogMonkeyPatch
   # We override this method so that we can resolve Umbrella Subscriptions differently
   def current_products
-    products = parent.team.billing_subscriptions.active.where.not(provider_subscription_type: "Billing::Umberlla::Subscription").map(&:included_prices).flatten.map(&:price).map(&:product)
-    umbrella_subscriptions = parent.team.billing_subscriptions.active.where(provider_subscription_type: "Billing::Umberlla::Subscription")
+    products = parent.team.billing_subscriptions.active.where.not(provider_subscription_type: "Billing::Umbrella::Subscription").map(&:included_prices).flatten.map(&:price).map(&:product)
+    umbrella_subscriptions = parent.team.billing_subscriptions.active.where(provider_subscription_type: "Billing::Umbrella::Subscription")
     umbrella_subscriptions.each do |us|
       uteam = us.provider_subscription.covering_team
-      products += uteam.billing_subscriptions.active.where.not(provider_subscription_type: "Billing::Umberlla::Subscription").map(&:included_prices).flatten.map(&:price).map(&:product)
+      products += uteam.billing_subscriptions.active.where.not(provider_subscription_type: "Billing::Umbrella::Subscription").map(&:included_prices).flatten.map(&:price).map(&:product)
     end
     products.any? ? products : free_products
   end
