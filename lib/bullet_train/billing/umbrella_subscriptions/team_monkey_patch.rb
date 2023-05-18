1ba974358c5d200a0c56945576abc6f129e6ecae
Jeremy Green <jeremy@octolabs.com>
2023-05-18 12:04:13 -0500

    Need to test limits against active covering subsriptions

--- lib/bullet_train/billing/umbrella_subscriptions/team_monkey_patch.rb
+++ lib/bullet_train/billing/umbrella_subscriptions/team_monkey_patch.rb
@@ -2,7 +2,11 @@ module BulletTrain::Billing::UmbrellaSubscriptions::TeamMonkeyPatch
   extend ActiveSupport::Concern
   prepended do
     has_many :billing_umbrella_subscriptions, class_name: "Billing::Umbrella::Subscription", dependent: :destroy, foreign_key: :team_id
-    has_many :billing_covered_subscriptions, class_name: "Billing::Umbrella::Subscription", dependent: :destroy, foreign_key: :covering_team_id
+    has_many :billing_covering_subscriptions, class_name: "Billing::Umbrella::Subscription", dependent: :destroy, foreign_key: :covering_team_id
+
+    def active_covering_subscriptions
+      billing_covering_subscriptions.joins(:generic_subscription).where("billing_subscriptions.status" => "active")
+    end
 
     def umbrella_subscription_cover_limt_reached?
       limiter = Billing::Limiter.new(self)
@@ -11,7 +15,7 @@ module BulletTrain::Billing::UmbrellaSubscriptions::TeamMonkeyPatch
       limits = limiter.current_products.first.limits
       if limits["billing/umbrella/subscriptions"]
         cover_limit = limits["billing/umbrella/subscriptions"]["cover"]["count"]
-        if billing_covered_subscriptions.count < cover_limit
+        if active_covering_subscriptions.count < cover_limit
           return false
         end
       end
