f8dcec268052638da564ef7562e3a848bb52d027
Jeremy Green <jeremy@octolabs.com>
2023-05-18 11:36:48 -0500

    A better definition of valid_teams, and a validation for covering limit

--- app/models/billing/umbrella/subscription.rb
+++ app/models/billing/umbrella/subscription.rb
@@ -3,6 +3,8 @@ class Billing::Umbrella::Subscription < ApplicationRecord
   belongs_to :covering_team, class_name: "Team"
   has_one :generic_subscription, class_name: "Billing::Subscription", as: :provider_subscription
 
+  validate :covering_team_cover_limit, on: :create
+
   after_create :create_generic_subscription
 
   def status=(new_status)
@@ -19,7 +21,28 @@ class Billing::Umbrella::Subscription < ApplicationRecord
   end
 
   def valid_teams
-    # TODO: We probably only want teams for which the current user is an admin
-    Current.user&.teams&.where&.not(id: Current.team&.id) || Team.none
+    # TODO: This is kinda messy, but does what we need. Ideally we wouldn't hard code the admin role here.
+    # And ideally we could do all of this as one big select, but my SQL foo is weak.
+
+    # an array to hold the IDs of teams that we consider valid
+    valid_team_ids = []
+
+    # First grab all the memberships for which the current user has the admin role
+    memberships = Current.user.memberships.where('role_ids ?& array[:keys]', keys: ['admin'])
+
+    # Now loop the memberships, and if the team has not reached the cover limit add the id to the array
+    memberships.each do |membership|
+      if !membership.team.umbrella_subscription_cover_limt_reached?
+        valid_team_ids.push membership.team.id
+      end
+    end
+
+    Team.where(id: valid_team_ids)
+  end
+
+  def covering_team_cover_limit
+    if(covering_team.umbrella_subscription_cover_limt_reached?)
+      errors.add(:base, "The team you selected is not allowed to cover any additional teams.")
+    end
   end
 end
--- lib/bullet_train/billing/umbrella_subscriptions/team_monkey_patch.rb
+++ lib/bullet_train/billing/umbrella_subscriptions/team_monkey_patch.rb
@@ -2,5 +2,20 @@ module BulletTrain::Billing::UmbrellaSubscriptions::TeamMonkeyPatch
   extend ActiveSupport::Concern
   prepended do
     has_many :billing_umbrella_subscriptions, class_name: "Billing::Umbrella::Subscription", dependent: :destroy, foreign_key: :team_id
+    has_many :billing_covered_subscriptions, class_name: "Billing::Umbrella::Subscription", dependent: :destroy, foreign_key: :covering_team_id
+
+    def umbrella_subscription_cover_limt_reached?
+      limiter = Billing::Limiter.new(self)
+      # TODO: This assumes that the covering team has only one subscription, because we seem to assume that BT teams
+      # only have one subscription. If that changes this will need to be tweaked.
+      limits = limiter.current_products.first.limits
+      if limits["billing/umbrella/subscriptions"]
+        cover_limit = limits["billing/umbrella/subscriptions"]["cover"]["count"]
+        if billing_covered_subscriptions.count < cover_limit
+          return false
+        end
+      end
+      return true
+    end
   end
 end
