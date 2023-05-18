f0ecbba14f0afc7fe2b916b86531fd54f435391c
Jeremy Green <jeremy@octolabs.com>
2023-05-18 16:00:30 -0500

    Don't show the umbrella subscriptions area for teams that aren't eligible to extend them

--- lib/bullet_train/billing/umbrella_subscriptions/team_monkey_patch.rb
+++ lib/bullet_train/billing/umbrella_subscriptions/team_monkey_patch.rb
@@ -25,5 +25,23 @@ module BulletTrain::Billing::UmbrellaSubscriptions::TeamMonkeyPatch
       end
       return true
     end
+
+    def can_extend_umbrella_subscriptions?
+      # First we have to check our recurssion bailout condition
+      if current_billing_subscription && current_billing_subscription.provider_subscription_type == "Billing::Umbrella::Subscription"
+        return false
+      end
+      limiter = Billing::Limiter.new(self)
+      # TODO: This assumes that the covering team has only one subscription, because we seem to assume that BT teams
+      # only have one subscription. If that changes this will need to be tweaked.
+      limits = limiter.current_products.first.limits
+      if limits["billing/umbrella/subscriptions"]
+        cover_limit = limits["billing/umbrella/subscriptions"]["cover"]["count"]
+        if cover_limit > 0
+          return true
+        end
+      end
+      return false
+    end
   end
 end
