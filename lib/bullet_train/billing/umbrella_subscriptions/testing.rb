40c4016ddcda17d9f51a97bce2858824cc4dd777
Jeremy Green <jeremy@octolabs.com>
2023-05-22 13:13:51 -0500

    getting started on a system test that can be used by the hosting app

---
+++ lib/bullet_train/billing/umbrella_subscriptions/testing.rb
@@ -0,0 +1,114 @@
+module BulletTrain::Billing::UmbrellaSubscriptions::Testing
+  extend ActiveSupport::Concern
+
+  included do
+  end
+
+  def setup
+    super
+    @jane = create :onboarded_user, first_name: "Jane", last_name: "Smith"
+    @team = @jane.teams.last
+    @other_team = @jane.teams.first
+
+    provider_subscription = Billing::External::Subscription.create!(team: @team)
+
+    subscription = Billing::Subscription.create!({
+      team: @team,
+      provider_subscription: provider_subscription,
+      status: 'active'
+    })
+
+    Billing::Subscriptions::IncludedPrice.create!(
+      subscription: subscription,
+      price_id: "band_monthly_2023",
+      quantity: 1
+    )
+  end
+
+  def default_umbrella_subscriptions_push_test(device_name, display_details)
+    resize_for(display_details)
+    login_as(@jane, scope: :user)
+    @jane.current_team = @team
+
+    visit root_path
+
+    # TODO: This is a little bit low-level for a system test but for now it's a sanity check.
+    # This should really probably be testsed somewhere else.
+    limiter = Billing::Limiter.new(@other_team)
+    assert_equal "free", limiter.current_products.first.id
+
+    assert page.has_content?("Teams")
+
+    within_team_menu_for_team(@jane.current_team) do
+      click_link "Billing"
+    end
+    assert page.has_content?("Umbrella Subscriptions")
+
+    click_on "Add New Umbrella Subscription"
+    assert page.has_content?("New Umbrella Subscription Details")
+
+    click_on "Create Umbrella Subscription"
+    assert page.has_content?("Umbrella Subscription was successfully created")
+
+    # TODO: This is a little bit low-level for a system test but for now it's a sanity check.
+    # This should really probably be testsed somewhere else.
+    limiter = Billing::Limiter.new(@other_team)
+    assert_equal "band", limiter.current_products.first.id
+
+    click_on "Edit"
+    assert page.has_content?("Umbrella Subscription Details")
+
+    click_on "Cancel this Umbrella Subscription"
+    assert page.has_content?("Umbrella Subscription was successfully updated")
+
+    # TODO: This is a little bit low-level for a system test but for now it's a sanity check.
+    # This should really probably be testsed somewhere else.
+    limiter = Billing::Limiter.new(@other_team)
+    assert_equal "free", limiter.current_products.first.id
+  end
+
+  def default_umbrella_subscriptions_pull_test(device_name, display_details)
+    resize_for(display_details)
+    login_as(@jane, scope: :user)
+    @jane.current_team = @other_team
+
+    visit root_path
+
+    # TODO: This is a little bit low-level for a system test but for now it's a sanity check.
+    # This should really probably be testsed somewhere else.
+    limiter = Billing::Limiter.new(@other_team)
+    assert_equal "free", limiter.current_products.first.id
+
+    assert page.has_content?("Teams")
+
+    within_team_menu_for_team(@jane.current_team) do
+      click_link "Billing"
+    end
+    assert page.has_content?("Upgrade")
+
+    click_on "Upgrade"
+    assert page.has_content?("You may be able to cover your team")
+
+    click_on "Click here to learn more."
+    assert page.has_content?("New Umbrella Subscription Details")
+
+    click_on "Create Umbrella Subscription"
+    assert page.has_content?("Umbrella Subscription was successfully created")
+
+    # TODO: This is a little bit low-level for a system test but for now it's a sanity check.
+    # This should really probably be testsed somewhere else.
+    limiter = Billing::Limiter.new(@other_team)
+    assert_equal "band", limiter.current_products.first.id
+
+    click_on "Manage"
+    assert page.has_content?("Umbrella Subscription Details")
+
+    click_on "Cancel this Umbrella Subscription"
+    assert page.has_content?("Umbrella Subscription was successfully updated")
+
+    # TODO: This is a little bit low-level for a system test but for now it's a sanity check.
+    # This should really probably be testsed somewhere else.
+    limiter = Billing::Limiter.new(@other_team)
+    assert_equal "free", limiter.current_products.first.id
+  end
+end
