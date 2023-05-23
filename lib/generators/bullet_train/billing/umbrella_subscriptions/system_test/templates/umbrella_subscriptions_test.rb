066220068460c0f27d771db00c57b287844c625a
Jeremy Green <jeremy@octolabs.com>
2023-05-23 11:37:00 -0500

    update our stub test

--- lib/generators/bullet_train/billing/umbrella_subscriptions/system_test/templates/umbrella_subscriptions_test.rb
+++ lib/generators/bullet_train/billing/umbrella_subscriptions/system_test/templates/umbrella_subscriptions_test.rb
@@ -4,6 +4,16 @@ require "bullet_train/billing/umbrella_subscriptions/testing"
 class UmbrellaSubscriptionsTest < ApplicationSystemTestCase
   include BulletTrain::Billing::UmbrellaSubscriptions::Testing
 
+  def setup
+    # You should make sure that these match producs that you have defined in config/models/products.yml.
+    # The pro level product needs to have umbrella subscriptions configured.
+    @pro_level_product_name = "pro"
+    @pro_level_price_id = "pro_monthly_2023"
+    @free_level_product_name = "free"
+
+    super
+  end
+
   @@test_devices.each do |device_name, display_details|
     test "default umbrella subscirptions push test on a #{device_name}" do
       default_umbrella_subscriptions_push_test(device_name, display_details)
