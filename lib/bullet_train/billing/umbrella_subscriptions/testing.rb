d905c592da06b312bd90adbc3725d22ef771a070
Jeremy Green <jeremy@octolabs.com>
2023-05-26 13:30:14 -0500

    Add some tests to cover an edge case

--- lib/bullet_train/billing/umbrella_subscriptions/testing.rb
+++ lib/bullet_train/billing/umbrella_subscriptions/testing.rb
@@ -13,6 +13,8 @@ module BulletTrain::Billing::UmbrellaSubscriptions::Testing
 
     @pro_level_product_name ||= "pro"
     @pro_level_price_id ||= "pro_monthly_2023"
+    @basic_level_product_name ||= "basic"
+    @basic_level_price_id ||= "basic_monthly_2023"
     @free_level_product_name ||= "free"
 
     provider_subscription = Billing::External::Subscription.create!(team: @team)
@@ -23,7 +25,7 @@ module BulletTrain::Billing::UmbrellaSubscriptions::Testing
       status: "active"
     })
 
-    Billing::Subscriptions::IncludedPrice.create!(
+    @included_price = Billing::Subscriptions::IncludedPrice.create!(
       subscription: subscription,
       price_id: @pro_level_price_id,
       quantity: 1
@@ -44,7 +46,6 @@ module BulletTrain::Billing::UmbrellaSubscriptions::Testing
 
     assert page.has_content?("Teams")
 
-    # within_team_menu_for_team(@jane.current_team) do
     within_team_menu_for(display_details) do
       click_link "Billing"
     end
@@ -73,6 +74,70 @@ module BulletTrain::Billing::UmbrellaSubscriptions::Testing
     # This should really probably be testsed somewhere else.
     limiter = Billing::Limiter.new(@other_team)
     assert_equal @free_level_product_name, limiter.current_products.first.id
+
+    # Now we're going to make sure that we do the right thing when the covering team
+    # changes their subscirption to a plan that doesn't include Umbrella Subscriptions
+
+    visit root_path
+
+    assert page.has_content?("Teams")
+
+    click_link @team.name
+    assert page.has_content?(@team.name)
+
+    within_team_menu_for(display_details) do
+      click_link "Billing"
+    end
+    assert page.has_content?("Subscriptions")
+
+    assert page.has_content?("Umbrella Subscriptions")
+
+    click_on "Add New Umbrella Subscription"
+    assert page.has_content?("New Umbrella Subscription Details")
+
+    click_on "Create Umbrella Subscription"
+    assert page.has_content?("Umbrella Subscription was successfully created")
+
+    # TODO: This is a little bit low-level for a system test but for now it's a sanity check.
+    # This should really probably be testsed somewhere else.
+    limiter = Billing::Limiter.new(@other_team)
+    assert_equal @pro_level_product_name, limiter.current_products.first.id
+
+    # Now we change the subscription for the covering team
+    @included_price.update(price_id: @basic_level_price_id)
+
+    # TODO: This is a little bit low-level for a system test but for now it's a sanity check.
+    # This should really probably be testsed somewhere else.
+    limiter = Billing::Limiter.new(@other_team)
+    assert_equal "free", limiter.current_products.first.id
+
+    # Now let's make sure the UI is doing the right thing.
+
+    visit root_path
+
+    assert page.has_content?("Teams")
+
+    click_link @other_team.name
+    assert page.has_content?(@other_team.name)
+
+    within_team_menu_for(display_details) do
+      click_link "Billing"
+    end
+    assert page.has_content?("Subscriptions")
+
+    assert page.has_content?("This Umbrella Subscription is currently inactive".upcase)
+
+    click_link "Manage"
+    assert page.has_content?("Umbrella Subscription Details")
+    assert page.has_content?("This Umbrella Subscription is currently inactive")
+
+    click_on "Cancel this Umbrella Subscription"
+    assert page.has_content?("Umbrella Subscription was successfully updated")
+    assert page.has_content?("Upgrade")
+
+    click_on "Upgrade"
+    assert page.has_content?("Subscribe")
+    assert !page.has_content?("You may be able to cover your team")
   end
 
   def default_umbrella_subscriptions_pull_test(device_name, display_details)
@@ -89,7 +154,6 @@ module BulletTrain::Billing::UmbrellaSubscriptions::Testing
 
     assert page.has_content?("Teams")
 
-    # within_team_menu_for_team(@jane.current_team) do
     within_team_menu_for(display_details) do
       click_link "Billing"
     end
--- lib/generators/bullet_train/billing/umbrella_subscriptions/system_test/templates/umbrella_subscriptions_test.rb
+++ lib/generators/bullet_train/billing/umbrella_subscriptions/system_test/templates/umbrella_subscriptions_test.rb
@@ -9,6 +9,8 @@ class UmbrellaSubscriptionsTest < ApplicationSystemTestCase
     # The pro level product needs to have umbrella subscriptions configured.
     @pro_level_product_name = "pro"
     @pro_level_price_id = "pro_monthly_2023"
+    @basic_level_product_name ||= "basic"
+    @basic_level_price_id ||= "basic_monthly_2023"
     @free_level_product_name = "free"
 
     super
